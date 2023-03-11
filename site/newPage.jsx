44f8fcd7fb79f94b39077e7388970d527ab6b8e0
MsrCoder <musa-surucu@mail.ohu.edu.tr>
2023-03-12 00:17:11 +0300

    updated the footer

--- site/newPage.jsx
+++ site/newPage.jsx
@@ -128,7 +128,7 @@ export default class App extends React.Component {
         <footer className="footer">
           <div className="container">
             <div className="footer-main">
-              <p className="footer-main-title">bi sn bekle bekliuoytm</p>
+              <p className="footer-main-title">bi sn bedsajdfnasfnsınga</p>
               <a href="https://github.com/eleme/element-react/issues" target="_blank" rel="noopener noreferrer" className="footer-main-link">{this.getLocale('misc.feedback')}</a>
               <a href="https://github.com/eleme/element-react/blob/master/CONTRIBUTING.md" target="_blank" rel="noopener noreferrer" className="footer-main-link">{this.getLocale('misc.contribution')}</a>
               <a href={`http://element.eleme.io/#/${this.state.locale}/component/${this.state.page}`} target="_blank" rel="noopener noreferrer" className="footer-main-link">Element</a>
